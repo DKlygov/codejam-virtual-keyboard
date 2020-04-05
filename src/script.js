bffd9abea2ff2970acea1524a4e97bc806d5909b
DKlygov <dklygov@gmail.com>
2020-04-05 22:09:13 +0600

    feat: add keypress function

--- src/script.js
+++ src/script.js
@@ -4,17 +4,17 @@ const ruKeysShifted = ['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')',
 const enKeysUnshifted = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Delete', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'Shift', '↑', 'Control', 'Meta', 'Alt', ' ', '←', '↓', '→'];
 const enKeysShifted = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 'Delete', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', 'CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 'Shift', '↑', 'Control', 'Meta', 'Alt', ' ', '←', '↓', '→'];
 // Generates all HTML document at once
+const container = document.createElement('div');
+const keyboardWindow = document.createElement('textarea');
+const keybed = document.createElement('div');
 
 function generateElements() {
-  const container = document.createElement('div');
   container.className = 'container';
   document.body.appendChild(container);
 
-  const keyboardWindow = document.createElement('textarea');
   keyboardWindow.className = 'keyboardWindow';
   container.appendChild(keyboardWindow);
 
-  const keybed = document.createElement('div');
   keybed.id = 'keybed';
   container.appendChild(keybed);
 
@@ -31,3 +31,9 @@ function generateKeys() {
 window.addEventListener('load', generateElements());
 
 window.addEventListener('load', generateKeys());
+
+keybed.addEventListener('keydown', keyPress());
+
+function keyPress () {
+    
+}
